8a3d9106f8b75b57d21679fd08099a2f238426cf
cchyung <chyungspice@gmail.com>
2020-04-02 19:36:01 -0600

    update db

--- models.js
+++ models.js
@@ -8,7 +8,15 @@ const database = new Sequelize({
 
 const Dish = database.define('dish', {
     id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: Sequelize.STRING, allowNull: false }  
+    name: { type: Sequelize.STRING, allowNull: false },
+    description: {type: Sequelize.STRING},
+    addons: {type: Sequelize.STRING},
+    allergens: {type: Sequelize.STRING},
+    glutenFreePossible: {type: Sequelize.BOOLEAN, defaultValue: false},
+    veganPossible: {type: Sequelize.BOOLEAN, defaultValue: false},
+    canRemove: {type: Sequelize.STRING},
+    notes: {type: Sequelize.STRING},
+    tableTalkPoints: {type: Sequelize.STRING}
 })
 
 module.exports = {
