ebdbeb35786ca1fee0af725f5fcd150f74566bf3
cchyung <chyungspice@gmail.com>
2020-04-27 19:50:25 -0600

    Guess its a port issue

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const { database, Dish, Tag } = require('./models')
 const { passport } = require('./controller')
 
-const port = process.env.SERVER_PORT || 3000
+const port = process.env.PORT || 3000
 
 const app = express()
 
