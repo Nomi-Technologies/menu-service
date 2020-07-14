d60599c9dc55d40da603fe12dd8b8f709f53a862
Zhehao /Tony/ Lu <tony1023lv@gmail.com>
2020-07-14 14:03:15 +0800

    Impl slug field endpoint

--- controller.js
+++ controller.js
@@ -499,17 +499,20 @@ const fetchAsset = async (req, res) => {
 };
 
 const publicDishList = (req, res) => {
-  let restaurantId = req.params.restaurantId;
+  let uniqueName = req.params.uniqueName;
   Dish.findAll({
     attributes: ["id", "name", "description", "addons", "canRemove"],
-    where: { restaurantId: restaurantId },
-    include: [{ model: Tag }, { model: Category }],
+    include: [
+      { model: Tag }, 
+      { model: Category }, 
+      { model: Restaurant, where: { uniqueName: uniqueName } }
+    ],
   })
     .then((data) => res.send(data))
-    .catch((err) =>
+    .catch((err) => {
       res.status(500).send({
         message: err.message || "An error occured while getting dishes list",
-      })
+      })}
     );
 };
 
