6c4096546122ab206f428589e23e57140b0d35ec
Conner Chyung <chyungspice@gmail.com>
2020-08-09 17:13:04 -0600

    fix bug with setting category on dish

--- controller.js
+++ controller.js
@@ -226,7 +226,7 @@ const createDish = (req, res) => {
   Dish.create(dishData)
     .then((dish) => {
       dish.setTags(req.body.dishTags).then(() => {
-        dish.setCategories([req.body.categoryId]).then(() => {
+        dish.setCategory(req.body.categoryId).then(() => {
           res.send(data);
         })
       })
