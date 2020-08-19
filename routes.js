08bf72e7caecd35b67edbab61ff86404e9337723
Conner Chyung <chyungspice@gmail.com>
2020-08-18 18:17:01 -0600

    redo csv parse to update menu structure

--- routes.js
+++ routes.js
@@ -88,6 +88,7 @@ module.exports = (app) => {
   router.get("/menus/:id", controller.getMenu);
   router.put("/menus/:id", controller.updateMenu);
   router.delete("/menus/:id", controller.deleteMenu);
+  router.post("/menus/:id/uploadCSV", controller.uploadMenuCSV)
   router.get("/all-menus", controller.getAllMenus);
   router.get("/user/details", controller.getUserDetails);
   router.put("/user/details", controller.updateUserDetails);
