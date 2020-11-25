21ca7dba63742067dac0e1931bf94238c0e10c57
clairepo <clairepo@usc.edu>
2020-11-25 10:56:42 -0800

    add migrations

---
+++ migrations/20201125185112-modify_users_add_new_fields.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    queryInterface.addColumn(
+      'User',
+      'restaurantGroupId',
+      {
+        type: Sequelize.UUID,
+        allowNull: false,
+      }
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    queryInterface.removeColumn('User', 'restaurantGroupId')
+  }
+};
