4e3ad4d2ed96d0710cd57aca7e36dd0f9c6d47a7
Gavin Bains <gbains@usc.edu>
2021-01-26 20:42:12 -0800

    feat: add dishes controllers

---
+++ src/controllers/dishes/getDish.js
@@ -0,0 +1,19 @@
+const dishLogic = require('../../logic/dishes');
+
+// gets restaurant information based on authentication
+async function getDish(req, res) {
+  const dishId = req.params.id;
+
+  try {
+    const dish = dishLogic.getDishById(dishId);
+    res.send(dish);
+  }
+  catch(err) {
+    console.error(err);
+    res.status(500).send({
+      message: err.message || `An error occured while getting dish with id==${dishId}`,
+    });
+  }
+};
+
+module.exports = getDish;
---
+++ src/controllers/dishes/updateDish.js
@@ -0,0 +1,22 @@
+const dishLogic = require('../../logic/dishes');
+
+async function updateDish(req, res) {
+  const dishId = req.params.id;
+  const newDetails = req.body;
+  
+  try {
+    const dish = await dishLogic.getDishById(dishId);
+    await dishLogic.updateDish(dish, newDetails);
+    res.status(200).send({
+      message: 'Dish updated successfully',
+    });
+  }
+  catch(err) {
+    console.error(err);
+    res.status(500).send({
+      message: err.message || `An error occured while updating dish with dishId=${dishId}`,
+    });
+  }
+};
+
+module.exports = updateDish;
