775f96fa2148683234adfb470aba3018e0cef28a
Gavin Bains <gbains@usc.edu>
2021-02-28 19:19:37 -0800

    test: make getUserDetails and checkEmail self sufficient

--- test/controller/users/checkEmail.js
+++ test/controller/users/checkEmail.js
@@ -3,6 +3,8 @@ const chaiHttp = require('chai-http');
 const sinon = require('sinon'); // eslint-disable-line no-unused-vars
 const sinonChai = require('sinon-chai');
 const app = require('../../../index.js');
+const { generateTestRestaurantData, deleteTestRestaurantById, createTestRestaurant } = require('../../utils/restaurants');
+const { generateTestUserData, deleteTestUserById, createTestUser } = require('../../utils/users');
 
 chai.use(chaiHttp);
 chai.use(sinonChai);
@@ -11,17 +13,26 @@ const { expect } = chai;
 
 const endpoint = '/api/user/check-email';
 
-const TEST_EMAIL_TAKEN = { email: 'admin@test.com' };
-const TEST_EMAIL_NOT_TAKEN = { email: 'random@email.com' };
+const TEST_RESTAURANT = generateTestRestaurantData();
+const TEST_USER = generateTestUserData({ restaurantId: TEST_RESTAURANT.id });
+const TEST_EMAIL_NOT_TAKEN = { email: Math.random().toString() };
 
 describe('controller.users.checkEmail', () => {
+  before(async () => {
+    await createTestRestaurant(TEST_RESTAURANT);
+    await createTestUser(TEST_USER);
+  });
   after(async () => {
+    await Promise.all([
+      deleteTestUserById(TEST_USER.id),
+      deleteTestRestaurantById(TEST_RESTAURANT.id),
+    ]);
     app.server.close();
   });
   it('Should return true if email is taken', async () => {
     const res = await chai.request(app)
       .get(endpoint)
-      .query(TEST_EMAIL_TAKEN);
+      .query(TEST_USER.email);
     expect(res).to.have.status(200);
     expect(res.body.taken).to.be.true;
   });
--- test/controller/users/getUserDetails.js
+++ test/controller/users/getUserDetails.js
@@ -4,6 +4,8 @@ const sinon = require('sinon'); // eslint-disable-line no-unused-vars
 const sinonChai = require('sinon-chai');
 const app = require('../../../index.js');
 const { authenticateTestUser } = require('../../utils/users');
+const { generateTestRestaurantData, deleteTestRestaurantById, createTestRestaurant } = require('../../utils/restaurants');
+const { generateTestUserData, deleteTestUserById, createTestUser } = require('../../utils/users');
 
 chai.use(chaiHttp);
 chai.use(sinonChai);
@@ -12,14 +14,21 @@ const { expect } = chai;
 
 const endpoint = '/api/user/details';
 
-const TEST_USER = {
-  email: 'admin@test.com',
-  password: 'password123',
-};
+const TEST_RESTAURANT = generateTestRestaurantData();
+const TEST_USER = generateTestUserData({ restaurantId: TEST_RESTAURANT.id });
+
 let token;
 
 describe('controller.users.getUserDetails', () => {
+  before(async () => {
+    await createTestRestaurant(TEST_RESTAURANT);
+    await createTestUser(TEST_USER);
+  });
   after(async () => {
+    await Promise.all([
+      deleteTestUserById(TEST_USER.id),
+      deleteTestRestaurantById(TEST_RESTAURANT.id),
+    ]);
     app.server.close();
   });
   context('Authenticated Requests', () => {
