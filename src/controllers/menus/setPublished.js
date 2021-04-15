2fea088770295c963320fc8c9e7113ab44723436
Ryan Addiego <raddiego@usc.edu>
2021-04-14 20:24:10 -0700

    fix: add missing setPublished.js file

---
+++ src/controllers/menus/setPublished.js
@@ -0,0 +1,25 @@
+const menuLogic = require('../../logic/menus');
+
+async function setPublished(req, res) {
+    const userRestaurantId = req.user.restaurantId;
+    const menuId = req.params.id;
+    const menuData = req.body;
+
+    try {
+        const menu = await menuLogic.getMenuById(menuId);
+        if (menu) {
+            await menuLogic.updateMenuById(menuData, menuId);
+            res.status(200).send({
+                message: 'update successful'
+            })
+        }
+    }
+    catch (err) {
+        res.status(500).send({
+            message: 'unable to update successfully'
+        })
+        console.error(err);
+    }
+}
+
+module.exports = setPublished;
