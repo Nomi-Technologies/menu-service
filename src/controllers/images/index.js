689bff4b8970db865e959ecee1037c13771dcc55
Gavin Bains <gbains@usc.edu>
2021-04-26 16:42:20 -0700

    refactor(images): clean up & image uploads to have public read access (#105)
    * refactor(controllers): create images controllers and logic, clean up code
    
    * feat: allow public-read on upload
    
    * fix: comment out passReqToCallback
    
    this line was written into the code but never a part of the object. im not sure if we need it at all
    
    * refactor: remove unused code
    
    Co-authored-by: Zhehao /Tony/ Lu <tony1023lv@gmail.com>

---
+++ src/controllers/images/index.js
@@ -0,0 +1,15 @@
+const getDishImage = require('./getDishImage');
+const getMenuImage = require('./getMenuImage');
+const getRestaurantImage = require('./getRestaurantImage');
+const uploadDishImage = require('./uploadDishImage');
+const uploadMenuImage = require('./uploadMenuImage');
+const uploadRestaurantImage = require('./uploadRestaurantImage');
+
+module.exports = {
+  getDishImage,
+  getMenuImage,
+  getRestaurantImage,
+  uploadDishImage,
+  uploadMenuImage,
+  uploadRestaurantImage,
+};
